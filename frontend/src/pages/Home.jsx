18a5edfdec61bd6e209baab5a66bd879d5c359f0
AkhileshRajan19 <akhisome@gmail.com>
2025-12-01 19:07:41 +0530

    Create Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import UploadBox from "../components/UploadBox";
+
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>FINSTOP</h1>
+      <UploadBox />
+    </div>
+  );
+}
